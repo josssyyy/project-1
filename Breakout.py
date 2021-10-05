281d2577f62f54885a59936afb050c51260ea624
derekdip <derekdip16@gmail.com>
2021-10-05 00:24:46 -0700

    tried to animate ball

--- Breakout.py
+++ Breakout.py
@@ -4,6 +4,7 @@
 This program (once you have finished it) implements the Breakout game
 """
 
+from tkinter.constants import TRUE
 from pgl import GWindow, GOval, GRect, GState
 import random
 
@@ -40,6 +41,8 @@ def breakout():
     gw = GWindow(GWINDOW_WIDTH, GWINDOW_HEIGHT)
     gs = GState()
     Grey_Paddle(gw)
+    populateBricks(gw)
+    setBallLocation(gw,.1,.1,80,PADDLE_Y-50)
     # You fill in the rest of this function along with any additional
     # helper and callback functions you need
 
@@ -48,31 +51,31 @@ def Grey_Paddle(gw):
     rect.set_color("Grey")
     rect.set_filled(True)
     gw.add(rect)
-    populateBricks(gw)
-    setBallLocation(gw,.2,.3,80,PADDLE_Y-50)
     # You fill in the rest of this function along with any additional
     # helper and callback functions you need
 
 def setBallLocation(gw,vx,vy,initialX,initialY):
-    boundLeft=0
-    boundTop=0
-    boundRight=GWINDOW_WIDTH
-    boundBottom=GWINDOW_HEIGHT
-    
-    vx=vx
-    vy=vy
-
+    #step function updates the ball location in the timer function 
+    def step():
+        oval.move(dx, dy)
+        if oval.get_x() <0 or oval.get_x()>GWINDOW_WIDTH-40:
+            oval.move(-dx,dy)
+            timer.stop()
+        elif oval.get_y() <0 or oval.get_y()>GWINDOW_HEIGHT-40:
+            timer.stop()
+    #initialize deltas and set the ball into canvas
+    dx = 1
+    dy = .4
     oval=GOval(initialX,initialY,BALL_SIZE,BALL_SIZE)
     oval.set_color('black')
     oval.set_filled(True)
     gw.add(oval)
-    if(initialX<boundLeft or initialX>boundRight or initialY<boundTop or initialY>boundBottom):
-        print("out of bounds")
-    else:
-        setBallLocation(gw,vx,vy,initialX+vx,initialY+vy)
+    timer = gw.set_interval(step, 20)
 
 def populateBricks(gw):
-    i=0
+    
+    #initalizing values for the bricks
+    columnCount=0
     totalBlocks=0
     color=['red','orange','green','#1F85DE','blue']
     colorRowCount=0
@@ -81,22 +84,29 @@ def populateBricks(gw):
     brickYLoc=20
     brickColor=color[0]
     while(totalBlocks<N_ROWS*N_COLS):
-        if(i==N_COLS):
-            i=0
+        if(columnCount==N_COLS):
+            #separate the rows and start at the beginning of a new row
+            columnCount=0
             colorRowCount+=1
             brickYLoc+=35
             brickXLoc=5
+            #every other row the color changes
+            #also makes sure index is not out of bounds
         if(colorRowCount==2):
             colorRowCount=0
             colorIndex+=1
             if(len(color)>colorIndex):
                 brickColor=color[colorIndex]
+
+        #initalizing the brick to be added to the canvas
         rect=GRect(brickXLoc,brickYLoc,BRICK_WIDTH-5,BRICK_HEIGHT)
         rect.set_color(brickColor)
         rect.set_filled(True)
+
+        #update for next brick and push current brick to canvas
         brickXLoc+=35
         totalBlocks+=1
-        i+=1
+        columnCount+=1
         gw.add(rect)
 
 
